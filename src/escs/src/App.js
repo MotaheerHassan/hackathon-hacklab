0573d882b5e2cd20b36123b0ead7c9e5d38fc5d3
avinashkj06 <93251313+avinashkj06@users.noreply.github.com>
2023-07-29 12:56:36 +0530

    changes for route

--- src/escs/src/App.js
+++ src/escs/src/App.js
@@ -1,11 +1,22 @@
+import ReactDOM from "react-dom/client";
+import {  Routes, Route, useNavigate } from "react-router-dom";
+import Login from "./Components/Login";
+import Welcome from "./Components/Welcome";
+import AddDetails from "./Components/AddDetails";
 import './App.css';
-import Login from './Components/Login';
+
 
 function App() {
+  const navigate = useNavigate();
   return (
-    <div className="App">
-      <Login/>
-    </div>
+      <div>
+        <button onClick={() => navigate(-1)}>go back</button>
+        <Routes>
+          <Route exact path="/" element={<Login/>}/>
+          <Route exact path="/welcome" element={<Welcome/>}/>
+          <Route exact path="/adddetails" element={<AddDetails/>}/>
+        </Routes>
+      </div>
   );
 }
 
