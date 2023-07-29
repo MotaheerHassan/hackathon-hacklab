03cce507d0ed782dd33601cb19eb73ab276f1f46
Kundan Kumar <101640547+KundanK-talentica@users.noreply.github.com>
2023-07-29 12:29:16 +0530

    changes

--- src/escs/src/Components/Welcome.js
+++ src/escs/src/Components/Welcome.js
@@ -1,27 +1,25 @@
 import React, { useState } from "react";
-import Welcome from './Welcome'
-import { useNavigate } from "react-router-dom";
-const Login = () => {
+
+const Welcome = () => {
 
   const [empId,setEmpId] = useState('')
-  const history = useNavigate();
+
   const handleSubmit = (e) =>{
     e.preventDefault()
     console.log(empId)
   }
-const callLandingPage = () => {
-    history('./Welcome')
-  };
+
 
   return (
     <div className="auth-form-container">
-      <form onSubmit={handleSubmit} className="login-form">
+      {/* <form onSubmit={handleSubmit} className="login-form">
         <label htmlFor="empId">Enter Employee ID</label>
         <input type="text" value={empId}  onChange={(e) => setEmpId(e.target.value)} name="empId" id="empId" placeholder="Employee Id"/>
-        <button type="submit" onClick = {callLandingPage}>Log In</button>
-      </form>
+        <button type="submit">Log In</button>
+      </form> */}
+      <p>Welcome to the landing Page!!!</p>
     </div>
   );
 };
 
-export default Login;
+export default Welcome;
